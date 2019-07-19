19ad5bdc95e3bf289040e754fd939ce4ac6fdf27
Genecode <genecode@yandex.ru>
2019-07-19 22:13:17 +0300

    add User#monthly_income_sum and User#monthly_expense_sum

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,19 @@ class User < ApplicationRecord
   validates :name, :address, :age, presence: true
   has_many :incomes
   has_many :expenses
+
+  def monthly_income_sum
+    incomes.where({ created_at: monthly_time_range }).sum(:amount)
+  end
+
+  def monthly_expense_sum
+    expenses.where({ created_at: monthly_time_range }).sum(:amount)
+  end
+
+  private
+
+  def monthly_time_range
+    current_time = DateTime.current
+    current_time.beginning_of_month..current_time
+  end
 end
